713a9faa90d61d6747aaea95a130d5f7b72112c6
Marco Baldo <62661915+marcobaldoroma@users.noreply.github.com>
2020-05-18 13:13:11 +0200

    387

--- third_exercise.r
+++ third_exercise.r
@@ -81,7 +81,7 @@ str(BCI)
 
 vegdist(BCI[1:2, ], method = "jaccard")           # calculate distance      # distance in species composition in triangle matrix. if every plot rappresent all the species pool I will got 1
 
-1 - vegdist(BCI[1:2, ], method = "jaccard")       # calculate similarity    
+vegdist(BCI[1:2, ], method = "jaccard")       # calculate similarity    
 
 vegdist(BCI[2:3, ], method = "jaccard")
 vegdist(BCI[1:3, ], method = "jaccard")
@@ -169,67 +169,284 @@ hist(sor_f)
 t.test(sor_d,sor_f)
 
 ### Exercise 1b, use "Stream" from BCI.env to split the dataset and repeat the calculations, plots and tests
-### you did in the previous exercise
+### you did in the previous exercise  lunedi 18/05
 
 
 data("BCI.env")
+
+head(BCI.env)
+BCI_no_stream <- BCI[BCI.env$Stream=="No",]
+BCI_stream <- BCI[BCI.env$Stream=="Yes",]
+
+dim(BCI_no_stream )
+dim(BCI_stream)
+
+sr_ns <-  specnumber(BCI_no_stream)
+  
+sr_s <-  specnumber(BCI_stream)
+
+par(mfrow = c(1,2))
+hist(sr_ns,
+     main = "")
+hist(sr_s,
+     main "")
+t.test(sr_ns, sr_s)
+
+jac_ns <- vegdist(BCI_no_stream, "jaccard")
+jac_s <- vegdist(BCI_stream, "jaccard")
+
+par(mfrow = c(1,2))
+hist(sr_ns,
+     main = "")
+hist(sr_s,
+     main "")
+
+t.test(sr_ns, sr_s)
+
+sor_ns <- vegdist(BCI_no_stream, "bray")
+sor_s <- vegdist(BCI_stream, "bray")
+
+par(mfrow = c(1,2))
+hist(sr_ns,
+     main = "")
+hist(sr_s,
+     main "")
+t.test(sr_ns, sr_s)
+
+
+
+(((((((((((((data("BCI.env")
+
+ 
+
+head(BCI.env)
+
+ 
+
+BCI_no_stream <- BCI[BCI.env$Stream == "No", ]
+BCI_stream <- BCI[BCI.env$Stream == "Yes", ]
+
+ 
+
+dim(BCI_no_stream)
+dim(BCI_stream)
+
+ 
+
+sr_ns <- specnumber(BCI_no_stream)
+sr_s <- specnumber(BCI_stream)
+
+ 
+
+par(mfrow = c(1, 2))
+hist(sr_ns,
+     main = "")
+hist(sr_s,
+     main = "")
+t.test(sr_ns, sr_s)
+
+ 
+
+jac_ns <- vegdist(BCI_no_stream, "jaccard")
+jac_s <- vegdist(BCI_stream, "jaccard")
+
+ 
+
+par(mfrow = c(1, 2))
+hist(jac_ns,
+     main = "")
+hist(jac_s,
+     main = "")
+t.test(jac_ns, jac_s)
+
+ 
+
+sor_ns <-vegdist(BCI_no_stream, "bray")
+sor_s <- vegdist(BCI_stream, "bray")
+
+ 
+
+par(mfrow = c(1, 2))
+hist(sor_ns,
+     main = "")
+hist(sor_s,
+     main = "")
+t.test(sor_ns, sor_s)
+             )))))))))))))))))))))))))))))))))))))))))))))))))))
+
+
+
+
+
+
+
 
 
 
 #stream or river
 #stream, or any stress, if it 
 
-plot(BCI.env$UTM.EW, BCI.env$UTM.NS, colour = rainbow(specnumber(BCI))) #error
+
 
 ????????????????????????????????????????????????????????????????????
 
 ### Partitioning beta diversity
 
+library(betapart)
+
 beta.multi(BCI)
+
+#$beta.SIM        Simpson dissimilarity= turnover
+#[1] 0.8840073
+#$beta.SNE        Nestedness
+#[1] 0.01347489
+#$beta.SOR        Sorensen 
+#[1] 0.8974822
+
 beta.multi(BCI[1:2, ])
 beta.multi(BCI[1:3, ])
 
 beta.pair(BCI[1:3, ])
 beta.pair(BCI)
+
 boxplot(beta.pair(BCI))
+par(nfrow = c(1,1))
+boxplot(beta.pair(BCI))
+
 boxplot(beta.pair(BCI),
         names = c("Turnover", "Nestedness", "Total beta"))
 
 par(mfrow = c(1, 2))
 boxplot(beta.pair(BCI),
+        y= "dissimilarity",
         names = c("Turnover", "Nestedness", "Total beta"),
         main = "Sorensen")
 boxplot(beta.pair(BCI, "jaccard"),
+        y= "dissimilarity",
         names = c("Turnover", "Nestedness", "Total beta"),
         main = "Jaccard")
 par(mfrow = c(1, 1))
 
+
+
+
+
 ### Exercise 2, calculate and plot beta components (for pairs) according to Jaccard for ceram.s and ceram.n in a multi-panel plot,
 ### before doing so inspect the two datasets and calculate overall beta components
 
-data(ceram.s)
+library(betapart)
+
+# ceram.s Cerambycidae from Southern and northern European Countries
+data(ceram.s)  
 data(ceram.n)
 
+head(ceram.n[1:10])
+head(ceram.s[1:10])
 
+beta.multi(ceram.n)
+beta.multi(ceram.s)
+
+beta.multi(ceram.n[1:10])
+beta.multi(ceram.s[1:10])
+
+beta.pair(ceram.n[1:10])
+beta.pair(ceram.n)
+
+beta.pair(ceram.s[1:10])
+beta.pair(ceram.s)
+
+par(mfrow = c(2, 2))
+boxplot(beta.pair(ceram.n),
+        ylab= "Sor dissimilarity",
+        names = c("Turnover", "Nestedness", "Total beta"),
+        main = " Cerambycidae N-EU Sorensen")
+boxplot(beta.pair(ceram.n, "jaccard"),
+        ylab = "Jac dissimilarity",
+        names = c("Turnover", "Nestedness", "Total beta"),
+        main = " Cerambycidae N-EU Jaccard")
+
+#par(mfrow = c(1, 2))
+boxplot(beta.pair(ceram.s),
+        ylab= "Sor dissimilarity",
+        names = c("Turnover", "Nestedness", "Total beta"),
+        main = "Cerambycidae S-EU Sorensen")
+boxplot(beta.pair(ceram.s, "jaccard"),
+        ylab = "Jac dissimilarity",
+        names = c("Turnover", "Nestedness", "Total beta"),
+        main = " Cerambycidae S-EU Jaccard")
+
+dev.off()
+
+    
+### Exercise 2b, do the same you did in exercise B for the following simulated dataset "m"
+### by using f to split the dataset into two subsets
+
+
+set.seed(42)
+m <- matrix(data = sample(0:1, 1000, replace = T), ncol = 20)
+f <- sample(c("A", "B"), 50, replace = T)
+
+m_A <- m[f=="A",]
+m_B <- m[f=="B",]
+
+par(mfrow = c(1, 2))
+boxplot(beta.pair(m_A, "jaccard"),
+        main = "m_A",
+        ylab = "Jaccard dissimilarity index",
+        ylim = c(0, 0.8),
+        names = c("Turnover", "Nestedness", "Total beta"))
+boxplot(beta.pair(m_B, "jaccard"),
+        main = "m_B",
+        ylim = c(0, 0.8),
+        names = c("Turnover", "Nestedness", "Total beta"))
+
+###################### set.seed(42)
+set.seed(42)
+m <- matrix(data = sample(0:1, 1000, replace = T), ncol = 20)
+f <- sample(c("A", "B"), 50, replace = T)
+
+m_a <- m[f == "A", ]
+m_b <- m[f == "B", ]
+
+par(mfrow = c(1, 2))
+boxplot(beta.pair(m_a, "jaccard"),
+        ylim = c(0, 1),
+        names = c("Turnover", "Nestedness", "Overall Beta"))
+boxplot(beta.pair(m_b, "jaccard"),
+        ylim = c(0, 1),
+        names = c("Turnover", "Nestedness", "Overall Beta"))
+par(mfrow = c(1, 1))
+beta.multi(m_a)
+beta.multi(m_b)
+ #######################################################
 
 ### Distance decay
+# possiamo usare il concetto e modello della distanza anche per esempio per la distanza tra i pattern di riflettanza della vegetazione!!
 
 data(BCI.env)
+head(BCI.env)
 
-spat.dist <- dist(BCI.env[, 1:2])
+spat.dist <- dist(BCI.env[, 1:2])    # packages SPAT statistical default pack. R. distance euclidea!!!
 
-dissim.BCI <- beta.pair(BCI)$beta.sim
+dissim.BCI <- beta.pair(BCI)$beta.sim   # we have to use one at time variable on beta.pair we decide sim=turnover
 
-plot(spat.dist, dissim.BCI, ylim = c(0.1, 0.6), xlim = c(0, max(spat.dist)))
-
-BCI.decay.exp <- decay.model(dissim.BCI, spat.dist, model.type = "exp", perm = 100)
+plot(spat.dist, dissim.BCI, ylim = c(0.1, 0.6), xlim = c(0, max(spat.dist)))   # plot the distance decay matrix, first we use spat.dist= distanza in X axis and dissim.BCI in Y axis, at the end we specify the limit of axis
+# rapper or other model you can use
+BCI.decay.exp <- decay.model(dissim.BCI, spat.dist, model.type = "exp", perm = 100)  # perm = permutated
 BCI.decay.exp
 
-BCI.decay.pow <- decay.model(dissim.BCI, spat.dist, model.type = "pow", perm = 100)
+BCI.decay.pow <- decay.model(dissim.BCI, spat.dist, model.type = "pow", perm = 100)  # we specify the dissimilarity, after the spatial distamce, the model.type, in the end permutation= in order to see significats patter
 BCI.decay.pow
+
+# important information 
+# $model  Call:  glm(formula = y ~ x, family = gaussian(link = "log"))
+#$pseudo.r.squared [1] 0.06687897  = AIC: -3843
 
 plot.decay(BCI.decay.exp, col = "red", remove.dots = TRUE, add = TRUE)
 plot.decay(BCI.decay.pow, col = "blue", remove.dots = TRUE, add = TRUE)
+
+## we fit different model to check the best model for our analysis, the model the we can use to better fit the relation on my informations
+# akaike acaico information criterio
 
 BCI.decay.exp$pseudo.r.squared
 BCI.decay.pow$pseudo.r.squared
@@ -237,7 +454,7 @@ BCI.decay.exp$model
 BCI.decay.pow$model
 
 ### Exercise 3, display distance decay patterns for the "mite" dataset (from vegan), fit the power
-### and the exponential model for the beta diversity according to 
+### and the exponential model for the overall beta diversity according to 
 ### Sorensen ($beta.sor component of beta.pair()) and evaluate the two models.
 ### Plot location can be found in "mite.xy", use the function data() to load the two matrices.
 ### Remember to transform the abundance matrix into a presence/absence matrix
@@ -246,16 +463,38 @@ BCI.decay.pow$model
 ?mite
 
 
+BCI.decay.exp <- decay.model(dissim.BCI, spat.dist, model.type = "exp", perm = 100)
+BCI.decay.exp
 
+BCI.decay.exp <- decay.model(dissim.BCI, spat.dist, model.type = "exp", perm = 100)
+BCI.decay.exp
 
+ 
 
+BCI.decay.pow <- decay.model(dissim.BCI, spat.dist, model.type = "pow", perm = 100)
+BCI.decay.pow
 
+ 
 
+plot.decay(BCI.decay.exp, col = "red", remove.dots = TRUE, add = TRUE)
+plot.decay(BCI.decay.pow, col = "blue", remove.dots = TRUE, add = TRUE)
 
+R^2 the greater the better
+AIC the smaller the better
 
+### Exercise 3, display distance decay patterns for the "mite" dataset (from vegan), fit the power
+### and the exponential model for the overall beta diversity according to 
+### Sorensen ($beta.sor component of beta.pair()) and evaluate the two models.
+### Plot location can be found in "mite.xy", use the function data() to load the two matrices.
+### Remember to transform the abundance matrix into a presence/absence matrix
+### before calculating beta diversity
 
+ 
 
+library(vegan)
+?mite
 
+remember to load vegan package before loading the dataset
 
 
 
@@ -297,10 +536,7 @@ rarecurve(dune_agg)
 
 ### BEWARE, these techniques are used for different, although similar, purposes and they hold
 ### DIFFERENT degrees of robustness!
-
-
-
-
+    
 
 
 
